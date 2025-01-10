e387b958e0a93530e37d73eeaea3a09c87220be2
Tonni Tielens <tonni@tonni.nl>
2025-01-10 10:30:00 +0100

    Do the actual writing

--- generate.py
+++ generate.py
@@ -2,6 +2,8 @@ import argparse
 import datetime
 
 from PyPDF2 import PdfReader, PdfWriter
+from PyPDF2.generic import NameObject, TextStringObject
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -15,7 +17,33 @@ def main():
     parser.add_argument('-d', '--date', required=True, help='Date')
 
     args = parser.parse_args()
-    print(args.input, args.output, args.name, args.course, args.date)
+
+    reader = PdfReader(args.input)
+    page = reader.pages[0]
+
+    all_objects = (annot.get_object() for annot in page['/Annots'])
+    input_objects = [obj for obj in all_objects if obj.get('/Subtype') == '/Widget' and obj.get('/T')]
+    field_object_mapping = {obj.get('/T'): obj for obj in input_objects}
+
+    # This is very specific to my PDF.
+    update_field(field_object_mapping, 'Text3', args.name)
+    update_field(field_object_mapping, 'Text4', args.course)
+    update_field(field_object_mapping, 'Datum11_af_date', args.date)
+
+    writer = PdfWriter()
+    writer.add_page(page)
+
+    # Write to output PDF
+    with open(args.output, 'wb') as output_file:
+        writer.write(output_file)
+
+    print('Done')
+
+
+def update_field(field_object_mapping, field_name, value):
+    field_object_mapping[field_name].update({
+        NameObject("/V"): TextStringObject(value)
+    })
 
 
 if __name__ == "__main__":
